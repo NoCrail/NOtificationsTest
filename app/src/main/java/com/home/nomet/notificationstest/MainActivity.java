7eae5aabb9d3f8d13fec23144339e4d46aeaf053
Владислав <vlad-s-d@ya.ru>
2017-06-25 21:03:41 +0300

    Initial commit

---
+++ app/src/main/java/com/home/nomet/notificationstest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.home.nomet.notificationstest;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtView;
+    private NotificationReciver nReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nReceiver = new NotificationReciver();
+        IntentFilter filter = new IntentFilter();
+        //filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        registerReceiver(nReceiver,filter);
+    }
+}
---
+++ app/src/main/java/com/home/nomet/notificationstest/NotificationListener.java
@@ -0,0 +1,41 @@
+package com.home.nomet.notificationstest;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.widget.Toast;
+
+/**
+ * Created by Nomet on 25.06.2017.
+ */
+public class NotificationListener extends
+        NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(
+            StatusBarNotification sbn) {
+        String a = String.valueOf(this.getActiveNotifications());
+        Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNotificationRemoved(
+            StatusBarNotification sbn) {
+
+    }
+}
+
+
+class NotificationReciver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+
+}
+
